5921d119488baaf106ad1ebf9c4cb1371c74fbf6
freedomField <47129891+freedomField@users.noreply.github.com>
2019-10-17 12:32:13 +0100

    Updates

---
+++ precache-manifest.ab374d47efec373f4acd06c8c682a4fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7e4c4a39f3103870fc69c3b99554d8",
+    "url": "/cat-dex/index.html"
+  },
+  {
+    "revision": "f21cc720cd9acc7330ec",
+    "url": "/cat-dex/static/css/main.a5289e89.chunk.css"
+  },
+  {
+    "revision": "f977fa80ff73af86627e",
+    "url": "/cat-dex/static/js/2.37e904de.chunk.js"
+  },
+  {
+    "revision": "f21cc720cd9acc7330ec",
+    "url": "/cat-dex/static/js/main.750a8915.chunk.js"
+  },
+  {
+    "revision": "90a049e4a9d72a6622a9",
+    "url": "/cat-dex/static/js/runtime-main.2b6f83bc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Cat-Dex/precache-manifest.aa32ff70a82f4edec6fb7e5d23dc6cd1.js"
+  "/cat-dex/precache-manifest.ab374d47efec373f4acd06c8c682a4fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cat-Dex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cat-dex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
