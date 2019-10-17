3bec7cf9daec64c91ec803cd246cdc23a9320a38
freedomField <47129891+freedomField@users.noreply.github.com>
2019-10-17 11:57:16 +0100

    Updates

---
+++ precache-manifest.c465a143cfccbc16579e108afdfb9f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0903eee5bb601ec522c685016e5046d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fa69916e89f632ebb36",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "353acc99f4593ed178dd",
+    "url": "/static/js/2.c06e30e6.chunk.js"
+  },
+  {
+    "revision": "5fa69916e89f632ebb36",
+    "url": "/static/js/main.d9503708.chunk.js"
+  },
+  {
+    "revision": "7ec7c317be8fffdb7a2b",
+    "url": "/static/js/runtime-main.3aa34bbf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
